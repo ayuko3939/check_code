597fb0a1ea05d1d78bc1c8c1f239f323f41536b9
Yoka Hasegawa <yoka309hase@gmail.com>
2025-02-28 22:46:48 +0900

    fix

--- srcs/nginx/init.sh
+++ srcs/nginx/init.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -e
 
-# WordPress の設定が完了しているか確認
-if [ ! -f /etc/nginx/ssl/localhost.crt ]; then
-  
-  echo "SSL 証明書が見つかりません。自己署名証明書を作成します。"
+cd /etc/nginx/ssl
 
-  cd /etc/nginx/ssl
-  # 秘密鍵の作成
-  openssl genrsa -out localhost.key 2048
+# 秘密鍵の作成
+openssl genrsa -out inception.key 2048
 
-  # 証明書署名要求 (CSR) の作成
-  openssl req -new -key localhost.key -out localhost.csr \
-      -subj "/C=JP/ST=Tokyo/L=Chiyoda/O=LocalDev/CN=localhost"
+# 証明書署名要求 (CSR) の作成
+openssl req -new -key inception.key -out inception.csr \
+    -subj "/C=JP/ST=Tokyo/L=Sinjuku/O=42Tokyo/CN=yohasega.42.fr"
 
-  # 自己署名証明書の作成 (有効期限 365 日)
-  openssl x509 -req -days 365 -in localhost.csr -signkey localhost.key -out localhost.crt
-fi
+# 自己署名証明書の作成 (有効期限 365 日)
+openssl x509 -req -days 365 -in inception.csr -signkey inception.key -out inception.crt
 
 exec nginx -g 'daemon off;'
